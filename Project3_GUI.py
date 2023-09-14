8b30a66b5c110810a9bbf47ac1b7cb75aef83234
Đặng Huỳnh Sơn <70623183+sondh1993@users.noreply.github.com>
2023-09-15 00:07:21 +0700

    Update Project3_GUI.py

--- Project3_GUI.py
+++ Project3_GUI.py
@@ -9,7 +9,7 @@ import time
 import re
 import streamlit as st
 # đọc file 
-df = pd.read_csv('Sendo_reviews.csv')
+data= pd.read_csv('Sendo_reviews.csv')
 
 # GUI
 st.title("Data Science Project")
@@ -37,28 +37,4 @@ if choice == 'Business Objective':
 elif choice == 'Build Project':
     st.subheader("Build Project")
     st.write("##### 1. Some data")
-    st.dataframe(data[['v2', 'v1']].head(3))
-    st.dataframe(data[['v2', 'v1']].tail(3))  
-    st.write("##### 2. Visualize Ham and Spam")
-    fig1 = sns.countplot(data=data[['v1']], x='v1')    
-    st.pyplot(fig1.figure)
-
-    st.write("##### 3. Build model...")
-    st.write("##### 4. Evaluation")
-    st.code("Score train:"+ str(round(score_train,2)) + " vs Score test:" + str(round(score_test,2)))
-    st.code("Accuracy:"+str(round(acc,2)))
-    st.write("###### Confusion matrix:")
-    st.code(cm)
-    st.write("###### Classification report:")
-    st.code(cr)
-    st.code("Roc AUC score:" + str(round(roc,2)))
-
-    # calculate roc curve
-    st.write("###### ROC curve")
-    fpr, tpr, thresholds = roc_curve(y_test, y_prob[:, 1])
-    fig, ax = plt.subplots()       
-    ax.plot([0, 1], [0, 1], linestyle='--')
-    ax.plot(fpr, tpr, marker='.')
-    st.pyplot(fig)
-
-    st.write("##### 5. Summary: This model is good enough for Ham vs Spam classification.")
+    st.dataframe(data.head(10))
