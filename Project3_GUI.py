df37348cbc0f93c933c7fab634b10a434da71ce5
Đặng Huỳnh Sơn <70623183+sondh1993@users.noreply.github.com>
2023-09-16 07:02:03 +0000

    ok

--- Project3_GUI.py
+++ Project3_GUI.py
@@ -69,48 +69,11 @@ elif choice== "EDA":
     # Get the top 10 customers with the most reviews
     top_customers = review_counts.head(10)
     st.bar_chart(top_customers)
-
-    df_positive = df_sub[df_sub['sentiment'] == 'positive']
-    df_negative = df_sub[df_sub['sentiment'] == 'negative']
-    positive_text = ' '.join(df_positive.dropna()['content'])
-    negative_text = ' '.join(df_negative.dropna()['content'])
-    wordcloud_positive = WordCloud(background_color='white').generate(positive_text)
-    wordcloud_negative = WordCloud(background_color='white').generate(negative_text)
-        # Get word frequencies from positive and negative word clouds
-    word_freq_positive = wordcloud_positive.words_
-    word_freq_negative = wordcloud_negative.words_
-
-    # Sort word frequencies in descending order
-    sorted_word_freq_positive = sorted(word_freq_positive.items(), key=lambda x: x[1], reverse=True)
-    sorted_word_freq_negative = sorted(word_freq_negative.items(), key=lambda x: x[1], reverse=True)
-
-    # Get the top 10 words and their frequencies for positive sentiment
-    top_words_positive = sorted_word_freq_positive[:10]
-    words_positive = [word[0] for word in top_words_positive]
-    frequencies_positive = [word[1] for word in top_words_positive]
-
-    # Get the top 10 words and their frequencies for negative sentiment
-    top_words_negative = sorted_word_freq_negative[:10]
-    words_negative = [word[0] for word in top_words_negative]
-    frequencies_negative = [word[1] for word in top_words_negative]
+ 
     # Display word cloud for positive text
     st.subheader('Word Cloud - Positive Sentiment')
-    st.image(wordcloud_positive.to_array(), use_column_width=True)
+    st.image('Word Cloud - Positive Sentiment.jpg', use_column_width=True)
 
     # Display word cloud for negative text
     st.subheader('Word Cloud - Negative Sentiment')
-    st.image(wordcloud_negative.to_array(), use_column_width=True)
-
-    # Create bar plots for word frequencies in positive and negative sentiment
-    st.subheader('Top 10 Words - Positive Sentiment')
-    st.bar_chart(frequencies_positive, labels=words_positive)
-
-    st.subheader('Top 10 Words - Negative Sentiment')
-    st.bar_chart(frequencies_negative, labels=words_negative)
-    # Display word cloud for positive text
-    st.subheader('Word Cloud - Positive Sentiment')
-    st.image('wordcloud_positive.png', use_column_width=True)
-
-    # Display word cloud for negative text
-    st.subheader('Word Cloud - Negative Sentiment')
-    st.image('wordcloud_negative.png', use_column_width=True)
+    st.image('Word Cloud - Negative Sentiment.png', use_column_width=True)
