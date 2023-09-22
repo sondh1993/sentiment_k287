b987ca8e1c0add1c6bfe74ca8a2b81949e261784
Đặng Huỳnh Sơn <70623183+sondh1993@users.noreply.github.com>
2023-09-22 09:13:21 +0700

    ok

--- Project3_GUI.py
+++ Project3_GUI.py
@@ -19,18 +19,11 @@ def missing_value_analysis(df):
     return miss_df
 
 def check_data(df, head=5, tail=5):
-    st.write("SHAPE\n Rows: {}".format(df.shape[0]))
-    st.write("Columns: {}".format(df.shape[1]))
-    st.write("~" * 82)
-    st.write("TYPES".center(82))
-    st.write(df.info())
-    st.write("~" * 82)
-    st.write("Missing Values Analysis".center(82))
+    st.write("Rows: {},Columns: {}".format(df.shape[0],df.shape[1]))
+    st.write("Missing Values Analysis")
     miss_df = missing_value_analysis(df)
     st.table(miss_df)
-    st.write("~" * 82)
-    st.write("Duplicated values".center(82))
-    st.write(df.duplicated().sum())
+    st.write("Duplicated values:",df.duplicated().sum())
 def file_to_dict(path):
     file = open(path, 'r', encoding = 'utf-8')
     lst = file.read().split('\n')
